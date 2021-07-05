279fe1dae55fbfbe19ef45ae488567bd414307d7
edujoao225 <edujoaooliveira@gmail.com>
2021-07-05 22:58:32 +0000

    primeira versão

---
+++ src/main/java/br/univille/eduardodacs2021/Eduardodacs2021Application.java
@@ -0,0 +1,13 @@
+package br.univille.eduardodacs2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eduardodacs2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eduardodacs2021Application.class, args);
+	}
+
+}
